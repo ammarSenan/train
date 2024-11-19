d159e104e57a27c5ec3f92077c0c82744b4e3cda
ammar senan <amaralazi@gmail.com>
2024-11-19 20:43:07 +0300

    hhh

---
+++ js/first_project/pro.js
@@ -0,0 +1,12 @@
+function input(){
+    var name1 = document.getElementById("textarea").value
+    document.getElementById("output").innerHTML = name1;
+}
+var test = 123;
+var n = test.toString();
+window.onclick = function na(){document.getElementById("output").innerHTML = typeof(n)}
+
+var name1 = "ammar               senan";
+console.log(name1.length);
+var n = name1.charCodeAt(3);
+console.log(n);
---
+++ js/js.js
@@ -0,0 +1,84 @@
+/*
+var filName = 'ammar' ;
+var age = 303;
+var aa=[1, 3, 4, 4];
+var oo={firsName: "ammar", age:22}
+document.getElementById("test").innerHTML = "<h1 style='color:red;'>"+filName+"</h1>";
+document.getElementById("test").innerHTML += oo.age;
+
+
+var aa=["one","two","three"];
+console.log(aa.indexOf("three"));
+*/
+// window.alert("ammar senan")
+
+// document.getElementById("sm").style.display = "none";
+// function fun(){
+//     document.getElementById("test").style.fontSize = "100px";
+//     document.getElementById("sm").style.display = "block";
+//    // window.print();
+
+// }
+// function fun2(){
+//     document.getElementById("test").style.fontSize = "20px";
+//     document.getElementById("sm").innerHTML = Date()
+//     }
+
+// var op= {};
+// op.name = "amamr";
+// op.num=123
+// console.log(op.name);
+// console.log(op.num);
+// var n = 1;
+// if(!isNaN(n)){
+//     window.alert("ammar is the best one");
+//     }
+
+// var con=0;
+// while(con<5){
+
+//     document.getElementById("test").innerHTML +=con+"</br>";
+//     con++;
+// }
+
+// for(var c=0; c<3; c++){
+
+//     document.getElementById("test").innerHTML +=c+"</br>";
+    
+// }
+///////////////////////////////////3Wschool////////////////////////////////////
+// alert(2**3);
+// document.getElementById("bi").style.fontSize= "200px";
+
+// //////////////////////////////////////////////////////////////////////////////
+// var Name="ammar";
+
+// function pN(na){
+//     alert("hello "+na);
+// }
+
+// pN(Name);
+
+// var n = prompt("name");
+// function cl(n){
+//     return Date - n;
+// }
+
+// document.getElementById("her").innerHTML = cl();
+
+
+
+(function (){
+    alert("hello");
+})();
+
+function p(...num){
+   let re=0;
+   for(let i =0; i<num.length;i++){
+    re += num[i];
+   }
+   document.write(re);
+}
+
+p(1,2,3,3,4,3,2,4,5,3,43,23);
+
